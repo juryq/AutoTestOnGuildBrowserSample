8e771d106eb48fa18378f537f9aecc8c87176ea6
Delaware <Delaware@DelawarematoiMac.local>
2012-12-05 12:09:56 +0800

    Add unit test

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Delaware on 12-12-5.
+//  Copyright (c) 2012年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,15 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Delaware on 12-12-4.
+//  Copyright (c) 2012年 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+
+
+@end
